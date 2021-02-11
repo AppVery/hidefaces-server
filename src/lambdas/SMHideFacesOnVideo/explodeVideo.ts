9f3a8a5c08e2fb4c072005f5ad31260a6c101293
Pablo Cianes <cianespablo@gmail.com>
2021-02-11 14:33:04 +0100

    🚩 ok step explodeVideo

--- src/lambdas/SMHideFacesOnVideo/explodeVideo.ts
+++ src/lambdas/SMHideFacesOnVideo/explodeVideo.ts
@@ -1,9 +1,10 @@
 import { generalFileService } from "../../services";
 import { VideoData } from "../../domain/interfaces/types";
+import { delay } from "../../utils/validations";
+import * as ffmpeg from "fluent-ffmpeg";
+import * as chokidar from "chokidar";
 import * as fs from "fs";
 
-/* eslint-disable @typescript-eslint/no-var-requires */
-const ffmpeg = require("fluent-ffmpeg");
 const bucketName = process.env.MAIN_BUCKET_NAME;
 
 type Event = {
@@ -13,6 +14,24 @@ type Event = {
       videoData: VideoData;
     };
   };
+};
+
+const initWatcher = async (path: string, id: string): Promise<void> => {
+  const watcher = chokidar.watch(path, {
+    ignored: /^\./,
+    persistent: true,
+    awaitWriteFinish: true,
+  });
+  watcher.on("add", async function (path: any) {
+    const fileBuffer = await fs.promises.readFile(path);
+    const filename = path.split("/")[3];
+    const Key = `videos/temporal/${id}/${filename}`;
+
+    if ("audio.mp3" === filename) return;
+
+    await generalFileService.saveBuffer(bucketName, Key, fileBuffer);
+    await fs.promises.unlink(path);
+  });
 };
 
 export const handler = async (event: Event): Promise<VideoData> => {
@@ -35,7 +54,9 @@ export const handler = async (event: Event): Promise<VideoData> => {
     console.log("mkdir", error);
   }
 
-  const execute = async () => {
+  initWatcher(tmpPath, videoData.id);
+
+  const explodeVideo = async () => {
     return new Promise((resolve, reject) => {
       ffmpeg(resultVideo.value)
         .on("end", function () {
@@ -44,28 +65,21 @@ export const handler = async (event: Event): Promise<VideoData> => {
         .on("error", function (err: any) {
           reject(err);
         })
-        //.output(audioPath)
-        //.output(framesPath)
-        //.run();
-        .save(audioPath);
+        .output(audioPath)
+        .output(framesPath)
+        .run();
     });
   };
 
-  await execute();
+  await explodeVideo();
 
   const audioBuffer = await fs.promises.readFile(audioPath);
   const audioS3Key = `videos/temporal/${videoData.id}/audio.mp3`;
-  /* eslint-disable no-console */
-  console.log("DEV 1", audioS3Key, audioBuffer);
 
-  const dev = await generalFileService.saveBuffer(
-    bucketName,
-    audioS3Key,
-    audioBuffer
-  );
+  await generalFileService.saveBuffer(bucketName, audioS3Key, audioBuffer);
 
-  /* eslint-disable no-console */
-  console.log("DEV 2", dev);
+  //wait chokidar finish with all frames
+  await delay(5);
 
   return videoData;
 };
--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,9 +1,12 @@
+export const delay = (seconds: number) =>
+  new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+
 export const hasValue = <T>(obj: unknown): obj is T => {
   return obj !== undefined && obj !== null;
 };
 
 const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
+  return typeof text === "string" || text instanceof String;
 };
 
 export const parseString = (label: string, data: unknown): string => {
@@ -16,7 +19,7 @@ export const parseString = (label: string, data: unknown): string => {
 
 const isEmail = (email: unknown): email is string => {
   const emailPattern = new RegExp(
-    /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i,
+    /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
   );
   return isString(email) && emailPattern.test(email);
 };
@@ -30,7 +33,7 @@ export const parseEmail = (label: string, data: unknown): string => {
 };
 
 const isValidVideoType = (type: unknown): type is string => {
-  const validVideoTypes = ['mp4', 'mkv'];
+  const validVideoTypes = ["mp4", "mkv"];
 
   return isString(type) && validVideoTypes.includes(type);
 };
@@ -40,7 +43,7 @@ export const parseExtension = (label: string, data: unknown): string => {
     throw new Error(`Incorrect or missing string: ${label}`);
   }
 
-  const type = data.replace(/^.*\./, '');
+  const type = data.replace(/^.*\./, "");
 
   if (!data || !isValidVideoType(type)) {
     throw new Error(`Incorrect or missing type of video`);
