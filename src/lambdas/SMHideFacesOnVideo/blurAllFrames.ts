d66e9a2d95cb9dcda4ee9a2a4e05e3177fd45c4b
Pablo Cianes <cianespablo@gmail.com>
2021-02-16 15:32:39 +0100

    🚩♻ split getFacesData step

--- src/lambdas/SMHideFacesOnVideo/blurAllFrames.ts
+++ src/lambdas/SMHideFacesOnVideo/blurAllFrames.ts
@@ -2,107 +2,38 @@ import { VideoData } from "../../domain/interfaces/types";
 import { generalFileService } from "../../services";
 import { Position } from "../../domain/interfaces/imageService";
 import { SharpImageService } from "../../services/SharpImageService";
-import { AwsRekognitionService } from "../../services/AwsRekognitionService";
-import {
-  RekognitionClient,
-  FaceDetail,
-  BoundingBox,
-} from "@aws-sdk/client-rekognition";
 import { Pixel } from "../../services/operations/PixelPO";
 
 const bucketName = process.env.MAIN_BUCKET_NAME;
 
-const awsRekognition = new RekognitionClient({});
-
-const awsRekognitionService = new AwsRekognitionService(awsRekognition);
-
 const imageService = new SharpImageService();
+
+type FramesData = {
+  facesPositions: [number, Position[]][];
+  mapper: [number, number][];
+};
 
 type Event = {
   Input: {
-    Payload: VideoData;
-  };
-};
-
-const getFrameS3Key = (id: string, i: number): string => {
-  return `videos/temporal/${id}/frame-${i}.png`;
-};
-
-const getFacesData = async (
-  videoData: VideoData
-): Promise<Map<number, FaceDetail[]>> => {
-  const baseS3key = `videos/temporal/${videoData.id}`;
-  const facesData = new Map<number, FaceDetail[]>();
-
-  for (let i = 1; i <= videoData.totalFrames; i += videoData.fps) {
-    const s3key = getFrameS3Key(videoData.id, i);
-    const result = await awsRekognitionService.getDataFacesImageS3(
-      s3key,
-      bucketName
-    );
-
-    if (result.isSuccess) {
-      const beforeFrameData = facesData.has(i - videoData.fps)
-        ? facesData.get(i - videoData.fps)
-        : [];
-      if (result.value.length > 0 && beforeFrameData.length === 0) {
-        facesData.set(i - videoData.fps, result.value);
-      }
-      if (result.value.length === 0 && beforeFrameData.length > 0) {
-        facesData.set(i, beforeFrameData);
-      } else {
-        facesData.set(i, result.value);
-      }
-    }
-  }
-
-  return facesData;
-};
-
-const getFacesPositions = (
-  frameWidth: number,
-  frameHeight: number,
-  BoundingBox: BoundingBox
-): Position => {
-  const INC_FACES_BOX = 2.2;
-  const { Top, Left, Width, Height } = BoundingBox;
-  const top = Math.floor(
-    Top * frameHeight +
-      (Height * frameHeight) / 2 -
-      (Height * frameHeight * INC_FACES_BOX) / 2
-  );
-  const left = Math.floor(
-    Left * frameWidth +
-      (Width * frameWidth) / 2 -
-      (Width * frameWidth * INC_FACES_BOX) / 2
-  );
-  return {
-    top: top > 0 ? top : 0,
-    left: left > 0 ? left : 0,
-    width: Math.floor(Width * frameWidth * INC_FACES_BOX),
-    height: Math.floor(Height * frameHeight * INC_FACES_BOX),
+    Payload: {
+      videoData: VideoData;
+      framesData: FramesData;
+    };
   };
 };
 
 export const handler = async (event: Event): Promise<VideoData> => {
   const operation = new Pixel(imageService);
-  const videoData = event.Input.Payload;
-  const facesData = await getFacesData(videoData);
+  const { videoData, framesData } = event.Input.Payload;
 
-  let frameWithData = 1;
+  const mapper: Map<number, number> = new Map(framesData.mapper);
+  const facesPositions: Map<number, Position[]> = new Map(
+    framesData.facesPositions
+  );
+
   for (let i = 1; i <= videoData.totalFrames; i++) {
-    frameWithData =
-      i === frameWithData || i < frameWithData + videoData.fps / 2
-        ? frameWithData
-        : frameWithData + videoData.fps;
-
-    const frameData = facesData.has(frameWithData)
-      ? facesData.get(frameWithData)
-      : facesData.get(frameWithData - videoData.fps);
-
-    const frameFacesPositions = frameData.map(({ BoundingBox }) => {
-      return getFacesPositions(videoData.width, videoData.height, BoundingBox);
-    });
+    const frameWithData = mapper.get(i);
+    const frameFacesPositions = facesPositions.get(frameWithData);
 
     if (frameFacesPositions.length === 0) {
       /* eslint-disable no-console */
@@ -110,7 +41,7 @@ export const handler = async (event: Event): Promise<VideoData> => {
       continue;
     }
 
-    const frameS3key = getFrameS3Key(videoData.id, i);
+    const frameS3key = `videos/temporal/${videoData.id}/frame-${i}.png`;
     const resultFrameBuffer = await generalFileService.getS3Buffer(
       bucketName,
       frameS3key
---
+++ src/lambdas/SMHideFacesOnVideo/getFacesData.ts
@@ -0,0 +1,125 @@
+import { VideoData } from "../../domain/interfaces/types";
+import { Position } from "../../domain/interfaces/imageService";
+import { AwsRekognitionService } from "../../services/AwsRekognitionService";
+import {
+  RekognitionClient,
+  FaceDetail,
+  BoundingBox,
+} from "@aws-sdk/client-rekognition";
+
+const bucketName = process.env.MAIN_BUCKET_NAME;
+
+const awsRekognition = new RekognitionClient({});
+
+const awsRekognitionService = new AwsRekognitionService(awsRekognition);
+
+type Event = {
+  Input: {
+    Payload: VideoData;
+  };
+};
+
+type FramesData = {
+  facesPositions: [number, Position[]][];
+  mapper: [number, number][];
+};
+
+type Response = {
+  videoData: VideoData;
+  framesData: FramesData;
+};
+
+const getFacesPositions = (
+  frameWidth: number,
+  frameHeight: number,
+  BoundingBox: BoundingBox
+): Position => {
+  const INC_FACES_BOX = 2.2;
+  const { Top, Left, Width, Height } = BoundingBox;
+  const top = Math.floor(
+    Top * frameHeight +
+      (Height * frameHeight) / 2 -
+      (Height * frameHeight * INC_FACES_BOX) / 2
+  );
+  const left = Math.floor(
+    Left * frameWidth +
+      (Width * frameWidth) / 2 -
+      (Width * frameWidth * INC_FACES_BOX) / 2
+  );
+  return {
+    top: top > 0 ? top : 0,
+    left: left > 0 ? left : 0,
+    width: Math.floor(Width * frameWidth * INC_FACES_BOX),
+    height: Math.floor(Height * frameHeight * INC_FACES_BOX),
+  };
+};
+
+export const handler = async (event: Event): Promise<Response> => {
+  const videoData = event.Input.Payload;
+  const interval = Math.floor(videoData.fps / 2);
+
+  const facesData = new Map<number, FaceDetail[]>();
+
+  for (let i = 1; i <= videoData.totalFrames; i += interval) {
+    const s3key = `videos/temporal/${videoData.id}/frame-${i}.png`;
+    const result = await awsRekognitionService.getDataFacesImageS3(
+      s3key,
+      bucketName
+    );
+
+    /* eslint-disable no-console */
+    console.log("frame", i, result.value.length);
+
+    if (result.isSuccess) {
+      const beforeFrameData = facesData.has(i - interval)
+        ? facesData.get(i - interval)
+        : [];
+      if (result.value.length > 0 && beforeFrameData.length === 0) {
+        facesData.set(i - interval, result.value);
+      }
+      if (result.value.length === 0 && beforeFrameData.length > 0) {
+        facesData.set(i, beforeFrameData);
+      } else {
+        facesData.set(i, result.value);
+      }
+    }
+  }
+
+  const facesPositions = new Map<number, Position[]>();
+
+  for (const [key, frameData] of facesData) {
+    if (frameData && frameData.length > 0) {
+      const frameFacesPositions = frameData.map(({ BoundingBox }) => {
+        return getFacesPositions(
+          videoData.width,
+          videoData.height,
+          BoundingBox
+        );
+      });
+      facesPositions.set(key, frameFacesPositions);
+    }
+  }
+
+  const mapper = new Map<number, number>();
+  let frameWithData = 1;
+
+  for (let i = 1; i <= videoData.totalFrames; i++) {
+    frameWithData =
+      i === frameWithData || i < frameWithData + interval
+        ? frameWithData
+        : frameWithData + interval;
+
+    const frameToMap = facesPositions.has(frameWithData)
+      ? frameWithData
+      : frameWithData - interval;
+
+    mapper.set(i, frameToMap);
+  }
+
+  const framesData: FramesData = {
+    facesPositions: [...facesPositions],
+    mapper: [...mapper],
+  };
+
+  return { videoData, framesData };
+};
