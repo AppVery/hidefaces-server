b77dbfbe9e6d72a74db15caaae3f3d216065f215
Pablo Cianes <cianespablo@gmail.com>
2021-02-19 10:07:31 +0100

    ✨ new video source to limit size and fps too big

--- src/lambdas/SMHideFacesOnVideo/getVideoMetadata.ts
+++ src/lambdas/SMHideFacesOnVideo/getVideoMetadata.ts
@@ -5,6 +5,7 @@ import * as fs from "fs";
 
 const bucketName = process.env.MAIN_BUCKET_NAME;
 const MAX_FPS = 30;
+const MAX_DIMENSION = 1920; //HD 1920x1080
 
 type Event = {
   Input: {
@@ -24,9 +25,11 @@ const makeCleanTemporalFolder = async (tmpPath: string): Promise<void> => {
   await fs.promises.mkdir(tmpPath);
 };
 
-const changeVideoSourceFPS = async (
+const changeVideoSource = async (
   videoData: VideoData,
-  s3key: string
+  s3key: string,
+  fps = 30,
+  percentage = 100
 ): Promise<string> => {
   const tmpPath = `/tmp/${videoData.id}`;
   const newVideoPath = `${tmpPath}/${videoData.filename}`;
@@ -39,7 +42,7 @@ const changeVideoSourceFPS = async (
     throw Error(`explodeVideo: ${resultVideo.error}`);
   }
 
-  const changeVideoFPS = async () => {
+  const changeVideo = async () => {
     return new Promise((resolve, reject) => {
       ffmpeg(resultVideo.value)
         .on("end", function () {
@@ -48,13 +51,14 @@ const changeVideoSourceFPS = async (
         .on("error", function (err: any) {
           reject(err);
         })
-        .outputFPS(MAX_FPS)
+        .size(`${percentage}%`)
+        .outputFPS(fps)
         .outputOptions("-movflags frag_keyframe+empty_moov")
         .save(newVideoPath);
     });
   };
 
-  await changeVideoFPS();
+  await changeVideo();
 
   //save video
   const videoBuffer = await fs.promises.readFile(newVideoPath);
@@ -87,27 +91,47 @@ export const handler = async (event: Event): Promise<Response> => {
 
   const metadata: any = await getMetadata();
   const video = metadata.streams[0];
+  const { width, height } = video;
   const fpsRate = video.r_frame_rate.split("/");
   const videoData = {
     id,
     filename,
     duration: video.duration,
-    width: video.width,
-    height: video.height,
+    width,
+    height,
     totalFrames: video.nb_frames,
     fps: Math.ceil(parseInt(fpsRate[0]) / parseInt(fpsRate[1])),
   };
 
-  if (!videoData.fps || videoData.fps > MAX_FPS) {
+  const haveWrongFPS = videoData.fps > MAX_FPS;
+
+  //TODOOOOOOOO get real dimensions with a frame like on explodeVideo
+  const haveWrongSizes =
+    !width || !height || width > MAX_DIMENSION || height > MAX_DIMENSION;
+
+  if (!videoData.fps || haveWrongFPS) {
     videoData.fps = MAX_FPS;
     videoData.totalFrames = Math.ceil(videoData.duration * MAX_FPS);
   }
 
   let newS3key = s3key;
 
-  if (videoData.fps > MAX_FPS) {
-    const videoS3Key = await changeVideoSourceFPS(videoData, s3key);
+  if (haveWrongFPS || haveWrongSizes) {
+    const fps = Math.min(videoData.fps, MAX_FPS);
+    const maxDimension = Math.max(width, height) || MAX_DIMENSION;
+    const sizePercentage = haveWrongSizes
+      ? Math.round((MAX_DIMENSION * 100) / maxDimension)
+      : 100;
+    const videoS3Key = await changeVideoSource(
+      videoData,
+      s3key,
+      fps,
+      sizePercentage
+    );
     newS3key = videoS3Key;
+    //check real dimensions on next step: explodeVideo
+    videoData.width = null;
+    videoData.height = null;
   }
 
   return {
